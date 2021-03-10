357916cea5afd891e63b2f6c24a04d7df859c2f3
Urs Voegele <unext@bluewin.ch>
2021-03-10 17:58:43 +0100

    application info changed

--- packer/provisioners/base-provisioning.sh
+++ packer/provisioners/base-provisioning.sh
@@ -21,5 +21,5 @@ PYTHON3_VERSION=$(python3 --version | awk  '{print $2}')
 PIP3_VERSION=$(pip3 --version | awk  '{print $2}')
 echo "# Installed application   "  > $application_file_path
 echo "***                       " >> $application_file_path
-echo "> python $PYTHON3_VERSION" >> $application_file_path
-echo "> pip $PIP3_VERSION" >> $application_file_path
+echo "> python3 $PYTHON3_VERSION" >> $application_file_path
+echo "> pip3 $PIP3_VERSION" >> $application_file_path
